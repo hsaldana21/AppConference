b7c2fdbc9488dc9b0e073d37e117964c0d7d71ff
Hector <hisraisc@gmail.com>
2020-05-08 13:04:01 -0500

    Create Session details Web Component
    Added new components: speakerCard and sessionDetails

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,32 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    sessionId;
+    state;
+
+    constructor() {
+        super();
+        this.state = 'list';
+        window.history.replaceState('list', null, '');
+        window.onpopstate = (event) => {
+            if (event.state) {
+                this.state = event.state;
+            }
+        };
+    }
+
+    // Backend logic to recieve the event that was fired by sessionList component
+    handleNavigate(event) {
+        this.sessionId = event.detail;
+        //This replaces the native 'pushState' and 'replaceState' functions on the browsers to navigate between states
+        this.state = 'details';
+        window.history.pushState('details', null);
+    }
+
+    get isStateList() {
+        return this.state === 'list';
+    }
+    get isStateDetails() {
+        return this.state === 'details';
+    }
+}
--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -8,11 +8,21 @@ export default class SessionList extends LightningElement {
         });
     }
 
-    //Event that gets calles when a user start typing on the search box element
+    //Event that gets called when a user start typing on the search box element
     handleSearchKeyInput(event) {
         const searchKey = event.target.value.toLowerCase();
         this.sessions = this.allSessions.filter((session) =>
             session.name.toLowerCase().includes(searchKey)
         );
+    }
+
+    //Event get called when a session was clicked
+    handleSessionClick(event) {
+        const index = event.currentTarget.dataset.index;
+        //This logic helps on dispatching the event to whichever component is listening
+        const navigateEvent = new CustomEvent('navigate', {
+            detail: this.sessions[index].id
+        });
+        this.dispatchEvent(navigateEvent);
     }
 }
